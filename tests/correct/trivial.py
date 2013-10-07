7694f26bafadd2e6ca782a596e847736155d66a8
Omar Ramadan <omar@fusi.io>
2013-10-07 13:00:13 -0700

    added tests, run with make check

--- tests/correct/trivial.py
+++ tests/correct/trivial.py
@@ -1,1 +1,20 @@
 print 1, 2, 3
+print "hello\t \n \\t \\n"
+print 'hello\t \n \\t \\n'
+print r"hello\t \n \\t \\n"
+print r'hello\t \n \\t \\n'
+
+print """hello\t \n \\t \\n"""
+print '''hello\t \n \\t \\n'''
+print r"""hello\t \n \\t \\n"""
+print r'''hello\t \n \\t \\n'''
+
+print """hello\t \n \\t \\n ' '' ''' " " \""" 
+"""
+print '''hello\t \n \\t \\n ' '' \''' " " """ 
+'''
+print r"""hello\t \n \\t \\n ' '' ''' " " \""" 
+"""
+print r'''hello\t \n \\t \\n ' '' \''' " " """ 
+'''
+
