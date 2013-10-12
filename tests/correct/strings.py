154d218179c8258e04c198bbdf51ede4f88935b3
Omar Ramadan <omar@fusi.io>
2013-10-12 13:01:47 -0700

    added string concat

--- tests/correct/strings.py
+++ tests/correct/strings.py
@@ -17,3 +17,8 @@ print r"""hello\t \n \\t \\n ' '' ''' " " \""" \012
 print r'''hello\t \n \\t \\n ' '' \''' " " """ \012
 '''
 
+print "hello" " " "world"
+print      "this is "  "a string"
+print      """this is
+a"""  " string"
+print      "this is" r' a string'
