8654dd938a2c90c359ec1487fc919f932547d64f
Ahmed Shamim <ahmedjshamim@gmail.com>
2013-10-14 13:58:51 -0700

    Fixed if_stmt, going to add type arguments on classes

---
+++ tests/correct/simpleif.py
@@ -0,0 +1,19 @@
+if ( 1 < 2 < 3 ):
+  print "1 < 2 < 3"
+else:
+  print "never gonna see this"
+
+if (False):
+  print "not this"
+else:
+  print "not this"
+
+if (False):
+  print "not this"
+else:
+  print "this"
+
+if (False):
+  print "not this"
+else:
+  print "not this"
