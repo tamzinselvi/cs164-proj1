ad8a096105d12171ae093829070034de636303b6
Omar Ramadan <omar@fusi.io>
2013-10-13 22:26:23 -0700

    added tests

--- tests/correct/subscription.py
+++ tests/correct/subscription.py
@@ -1,2 +1,11 @@
-X[A,B,C]
-X[A,92-1,a[0]]
+class Test:
+  def __getitem__(self, index):
+    return index
+
+A = 1
+B = 2**4 + 8 % 3
+a = [0,1,2]
+
+x = Test()
+print x[A,B,"hello"]
+print x[A,92-1,a[0]]
