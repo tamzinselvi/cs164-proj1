104cdebfd459fbd69f8cb2311ca5b723fdda693e
Ahmed Shamim <ahmedjshamim@gmail.com>
2013-10-13 23:03:14 -0700

    Fixed slicing test. Now working

--- tests/correct/slicing.py
+++ tests/correct/slicing.py
@@ -1,16 +1,10 @@
-class BullshitWorkaround:
-  def a():
-    return [1,2,3,4,5,6,7,8,9,10]
+a = [1,2,3,4,5,6,7,8,9,10]
+b = 4
+c = 7
 
-  def b():
-    return 4
-
-  def c():
-    return 7
-
-BullshitWorkaround.a()[1:4]
-BullshitWorkaround.a()[:3]
-BullshitWorkaround.a()[BullshitWorkaround.b():BullshitWorkaround.c()]
-BullshitWorkaround.a()[1+3:]
-BullshitWorkaround.a()[1*4:999-993]
+a[1:4]
+a[:3]
+a[b:c]
+a[1+3:]
+a[1*4:999-993]
 
